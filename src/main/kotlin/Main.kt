4a18dfe692d3198f4a6f9fd7fe20f886f37963f7
hongjinkwon <hjk0472@gmail.com>
2024-04-25 19:45:11 +0900

    🔬Feat: 추가 계산기 선택 옵션, 제작 계산기 기능 함수 틀

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,32 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("메뉴를 확인 후, 번호를 입력해주세요.")
+    println("1.더하기 / 2.빼기 / 3.곱하기 / 4.몫 구하기/ 5.나머지 구하기")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val selectedMenu = readLine()
+
+    when(selectedMenu) {
+        "1" -> addCalculator()
+        "2" -> subtractCalculator()
+        "3" -> multiplyCalculator()
+        "4" -> divideCalculator()
+        "5" -> RemainderCalculator()
+        "" -> println("값을 입력하지 않았습니다. 다시 실행해주세요.")
+        else -> println("잘못된 값을 입력했습니다. 다시 실행해주세요.")
+    } }
+
+fun addCalculator() {
+}
+
+fun subtractCalculator() {
+}
+
+fun divideCalculator()
+}
+
+fun multiplyCalculator() {
+}
+
+fun RemainderCalculator() {
 }
