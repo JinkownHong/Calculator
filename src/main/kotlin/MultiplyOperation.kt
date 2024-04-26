4c9a498b6a2a79b18cc6ba74efdffe5f8238eb88
hongjinkwon <hjk0472@gmail.com>
2024-04-26 11:27:13 +0900

    🚑Feat: 예외 처리 MultiplyOperation class
    숫자 미입력 시, 오류 캐치하여 숫자 입력할 수 있도록 설정

--- src/main/kotlin/MultiplyOperation.kt
+++ src/main/kotlin/MultiplyOperation.kt
@@ -1,17 +1,42 @@
 package org.example
+
 import Calculator
 
 class MultiplyOperation {
     fun multiplyCalculator() {
-        println("곱하기 연산을 실행합니다.")
-        println("하나의 숫자를 입력해주세요.")
-
-        val number1 = readln().toLong()
-
-        println("곱하기 연산할 숫자를 입력해주세요.")
-        val number2 = readln().toLong()
-
-        val calculator = Calculator()
-        println("값: ${calculator.multiplyOperate(number1, number2)}")
+        getMultiplyNumber()
     }
+}
+
+fun getMultiplyNumber() {
+    println("곱하기 연산을 실행합니다.")
+    println("하나의 숫자를 입력해주세요.")
+
+    var isValidInput = false
+    var number1: Long = 0
+    var number2: Long = 0
+
+    while (!isValidInput) {
+        try {
+            number1 = readln().toLong()
+            isValidInput = true
+        } catch (e: Exception) {
+            println("숫자만 입력할 수 있습니다.")
+        }
+    }
+
+    var isValidInput2 = false
+
+    println("곱하기 연산할 숫자를 입력해주세요.")
+    while (!isValidInput2) {
+        try {
+            number2 = readln().toLong()
+            isValidInput2 = true
+        } catch (e: Exception) {
+            println("숫자만 입력할 수 있습니다.")
+        }
+    }
+
+    val calculator = Calculator()
+    println("값: ${calculator.multiplyOperate(number1, number2)}")
 }
